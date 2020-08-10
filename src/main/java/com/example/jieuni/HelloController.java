055f518b2463049c86f8fae099e962afb37e22d8
Jieun <jieunijeong9@anver.com>
2020-08-10 21:43:25 +0900

    Initial commit

---
+++ src/main/java/com/example/jieuni/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.jieuni;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hi(Model model){
+        model.addAttribute("hothot", "덥노");
+        return "first";
+    }
+}
